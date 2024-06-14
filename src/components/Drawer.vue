af1309c8d5521395ae073d048270fddd12703bb4
Chipernes <nikonprofiz@gmail.com>
2024-06-14 22:44:45 +0300

    feat: added component for information, implemented in the cart

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -2,6 +2,7 @@
 
 import DrawerHead from "./DrawerHead.vue";
 import CartItemList from "./CartItemList.vue";
+import InfoBlock from "@/components/InfoBlock.vue";
 
 const emit = defineEmits(['createOrder']);
 
@@ -16,22 +17,32 @@ defineProps({
   <div class="bg-white w-96 h-full fixed right-0 top-0 z-20 p-8">
     <DrawerHead/>
 
-    <CartItemList/>
+    <div v-if="!totalPrice" class="flex h-full items-center">
+      <InfoBlock
+          title="Cart is empty"
+          description="Add at least one pair of sneakers to place your order."
+          image-url="/package-icon.png"
+      />
+    </div>
 
-    <div class="flex flex-col mt-7">
-      <div class="flex gap-2">
-        <span>Total:</span>
-        <div class="flex-1 border-b border-dashed"></div>
-        <b>${{ totalPrice }}</b>
+    <div v-else>
+      <CartItemList/>
+
+      <div class="flex flex-col mt-7">
+        <div class="flex gap-2">
+          <span>Total:</span>
+          <div class="flex-1 border-b border-dashed"></div>
+          <b>${{ totalPrice }}</b>
+        </div>
+
+        <button
+            :disabled="buttonDisabled"
+            @click="emit('createOrder')"
+            class="mt-4 bg-lime-500 w-full rounded-xl py-3 text-white disabled:bg-slate-300 hover:bg-lime-600 transition active:bg-lime-700"
+        >
+          Create order
+        </button>
       </div>
-
-      <button
-          :disabled="buttonDisabled"
-          @click="emit('createOrder')"
-          class="mt-4 bg-lime-500 w-full rounded-xl py-3 text-white disabled:bg-slate-300 hover:bg-lime-600 transition active:bg-lime-700"
-      >
-        Create order
-      </button>
     </div>
   </div>
 </template>
