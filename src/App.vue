dfa0ad94724d8d1eb56769a7bccf9b0e2e909510
Chipernes <nikonprofiz@gmail.com>
2024-06-16 12:18:47 +0300

    feat: added checkout status to the cart

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { computed, provide, ref, watch } from 'vue'
 import type { Ref } from 'vue'
-import axios from 'axios'
 
 import Header from '@/components/Header.vue'
 import Drawer from '@/components/Drawer.vue'
@@ -17,14 +16,10 @@ type SneakersItem = {
 }
 
 const cart: Ref<Array<SneakersItem>> = ref([])
-const isCreatingOrder = ref(false)
 
 const drawerOpen = ref(false)
 
 const totalPrice = computed(() => cart.value.reduce((acc, item) => acc + item.price, 0))
-
-const cartIsEmpty = computed(() => cart.value.length === 0)
-const cartButtonDisabled = computed(() => isCreatingOrder.value || cartIsEmpty.value)
 
 const closeDrawer = () => {
   drawerOpen.value = false
@@ -42,24 +37,6 @@ const addToCart = (item: SneakersItem) => {
 const removeFromCart = (item: SneakersItem) => {
   cart.value.splice(cart.value.indexOf(item), 1)
   item.isAdded = false
-}
-
-const createOrder = async () => {
-  try {
-    isCreatingOrder.value = true
-    const { data } = await axios.post('https://058e50087728bbd6.mokky.dev/orders', {
-      items: cart.value,
-      totalPrice: totalPrice.value
-    })
-
-    cart.value = []
-
-    return data
-  } catch (err) {
-    console.log(err)
-  } finally {
-    isCreatingOrder.value = false
-  }
 }
 
 watch(
@@ -82,12 +59,7 @@ provide('cart', {
 </script>
 
 <template>
-  <Drawer
-    v-if="drawerOpen"
-    :total-price="totalPrice"
-    @create-order="createOrder"
-    :button-disabled="cartButtonDisabled"
-  />
+  <Drawer v-if="drawerOpen" :total-price="totalPrice" />
 
   <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
     <Header :total-price="totalPrice" @open-drawer="openDrawer" />
--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,14 +1,63 @@
 <script setup lang="ts">
+import axios from 'axios'
+import { computed, ref, inject } from 'vue'
+import type { Ref } from 'vue'
+
 import DrawerHead from './DrawerHead.vue'
 import CartItemList from './CartItemList.vue'
 import InfoBlock from '@/components/InfoBlock.vue'
 
-const emit = defineEmits(['createOrder'])
+type SneakersItem = {
+  id: number
+  title: string
+  imageUrl: string
+  price: number
+  isFavorite: boolean
+  favoriteId: number
+  isAdded: boolean
+}
 
-defineProps({
-  totalPrice: Number,
-  buttonDisabled: Boolean
+type Cart = {
+  cart: Ref<Array<SneakersItem>>
+}
+
+const isCreating = ref(false)
+const orderId = ref(null)
+
+const props = defineProps({
+  totalPrice: Number
 })
+
+const cartActions = inject<Cart>('cart')
+
+if (!cartActions) {
+  throw new Error('cartActions injection failed')
+}
+
+const { cart } = cartActions
+
+const createOrder = async () => {
+  try {
+    isCreating.value = true
+    const { data } = await axios.post('https://058e50087728bbd6.mokky.dev/orders', {
+      items: cart.value,
+      totalPrice: props.totalPrice
+    })
+
+    cart.value = []
+
+    orderId.value = data.id
+
+    return data
+  } catch (err) {
+    console.log(err)
+  } finally {
+    isCreating.value = false
+  }
+}
+
+const cartIsEmpty = computed(() => cart.value.length === 0)
+const buttonDisabled = computed(() => isCreating.value || cartIsEmpty.value)
 </script>
 
 <template>
@@ -16,11 +65,19 @@ defineProps({
   <div class="bg-white w-96 h-full fixed right-0 top-0 z-20 p-8">
     <DrawerHead />
 
-    <div v-if="!totalPrice" class="flex h-full items-center">
+    <div v-if="!totalPrice || orderId" class="flex h-full items-center">
       <InfoBlock
+        v-if="!totalPrice && !orderId"
         title="Cart is empty"
         description="Add at least one pair of sneakers to place your order."
         image-url="/package-icon.png"
+      />
+
+      <InfoBlock
+        v-if="orderId"
+        title="Order is processed!"
+        :description="`Your order № ${orderId}. A manager will contact you soon.`"
+        image-url="/order-success-icon.png"
       />
     </div>
 
@@ -36,7 +93,7 @@ defineProps({
 
         <button
           :disabled="buttonDisabled"
-          @click="emit('createOrder')"
+          @click="createOrder"
           class="mt-4 bg-lime-500 w-full rounded-xl py-3 text-white disabled:bg-slate-300 hover:bg-lime-600 transition active:bg-lime-700"
         >
           Create order
